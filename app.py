f5f91248e3122a60a32439a68fb7988247fd4acf
Malthus Viorel <Malthus.nana@estiam.com>
2024-03-27 16:31:13 +0100

    4

--- app.py
+++ app.py
@@ -2,15 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from src.fetch_data import load_data_from_lag_to_today
-from src.process_data import col_date, col_donnees, main_process, fic_export_data, calculate_daily_average, calculate_daily_min_max
+from src.process_data import col_date, col_donnees, main_process, fic_export_data, calculate_daily_average, calculate_daily_min_max, count_empty_periods
 import logging
 import os
 import glob
-
-def remove_data(df: pd.DataFrame, last_n_samples: int = 4*3): 
-       # df: pd.DataFrame = pd.read_csv(fic_export_data)    
-       return df.iloc[:-last_n_samples]   
-        # df.to_csv(fic_export_data, index=False)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -30,12 +25,11 @@ st.title("Data Visualization App")
 @st.cache_data(ttl=15 * 60)
 def load_data(lag_days: int):
     load_data_from_lag_to_today(lag_days)
-    daily_avg, daily_min_max = main_process()
+    daily_avg, daily_min_max, empty_periods_count = main_process()
     data = pd.read_csv(fic_export_data, parse_dates=[col_date])
-    return data, daily_avg, daily_min_max
+    return data, daily_avg, daily_min_max, empty_periods_count
 
-df, daily_avg, daily_min_max = load_data(LAG_N_DAYS)
-df = remove_data(df, last_n_samples=4*24)
+df, daily_avg, daily_min_max, empty_periods_count = load_data(LAG_N_DAYS)
 
 st.subheader("Line Chart of Numerical Data Over Time")
 numerical_column = col_donnees
@@ -53,3 +47,7 @@ st.subheader("Daily Minimum and Maximum Consumption over the Last 7 Days")
 # Display daily minimum and maximum consumption as numbers
 st.write("Consommation quotidienne minimale et maximale sur les 7 derniers jours :")
 st.write(daily_min_max)
+
+st.subheader("Number of Empty Periods")
+st.write("Nombre de périodes sans données :")
+st.write(empty_periods_count)
--- src/process_data.py
+++ src/process_data.py
@@ -10,12 +10,12 @@ col_donnees: str = "consommation"
 cols: List[str] = [col_date, col_donnees]
 fic_export_data: str = "data/interim/data.csv"
 
+SEUIL_TEMPS_ENTRE_DATES: int = 3600  # Seuil de 1 heure (en secondes)
 
 def load_data():
     list_fic: list[str] = [Path(e) for e in glob.glob("data/raw/*json")]
     list_df: list[pd.DataFrame] = []
     for p in list_fic:
-        # list_df.append(pd.read_json(p))
         with open(p, "r") as f:
             dict_data: dict = json.load(f)
             df: pd.DataFrame = pd.DataFrame.from_dict(dict_data.get("results"))
@@ -26,13 +26,13 @@ def load_data():
 
 
 def format_data(df: pd.DataFrame):
-    # typage
+    # Typage
     df[col_date] = pd.to_datetime(df[col_date])
-    # ordre
+    # Ordre
     df = df.sort_values(col_date)
-    # filtrage colonnes
+    # Filtrage des colonnes
     df = df[cols]
-    # dédoublonnage
+    # Dédoublonnage
     df = df.drop_duplicates()
     return df
 
@@ -59,8 +59,22 @@ def main_process():
     df = format_data(df)
     daily_avg = calculate_daily_average(df)
     daily_min_max = calculate_daily_min_max(df)
+    empty_periods_count = count_empty_periods(df)
     export_data(df, fic_export_data)
-    return daily_avg, daily_min_max
+    return daily_avg, daily_min_max, empty_periods_count
+
+
+def count_empty_periods(df: pd.DataFrame):
+    # Calculer la différence entre chaque date pour détecter les périodes vides
+    df['time_diff'] = df[col_date].diff().dt.total_seconds()
+    
+    # Identifier les périodes vides en vérifiant si le temps entre les dates est supérieur à un certain seuil
+    empty_periods = df[df['time_diff'] > SEUIL_TEMPS_ENTRE_DATES]
+    
+    # Compter le nombre de périodes vides
+    empty_periods_count = len(empty_periods)
+    
+    return empty_periods_count
 
 
 if __name__ == "__main__":
