dc0f50c6415f3e72548fb991d5927dea40fceea8
Malthus Viorel <Malthus.nana@estiam.com>
2024-03-27 16:35:03 +0100

    5

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ import logging
 import os
 import glob
 
+def remove_data(df: pd.DataFrame, last_n_samples: int = 4*3):   
+     # df: pd.DataFrame = pd.read_csv(fic_export_data)
+         return df.iloc[:-last_n_samples]   
+          # df.to_csv(fic_export_data, index=False)
+
 logging.basicConfig(level=logging.INFO)
 
 LAG_N_DAYS: int = 7
@@ -38,6 +43,7 @@ st.plotly_chart(fig)
 
 st.subheader("Average Consumption per Day of the Week")
 
+df = remove_data(df, last_n_samples=4*24)
 # Create a bar chart for daily average consumption
 fig_bar = px.bar(daily_avg, x='day_of_week', y=col_donnees, title='Moyenne de la consommation par jour de la semaine')
 st.plotly_chart(fig_bar)
